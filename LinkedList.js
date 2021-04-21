c70ce26a2d4fe7fdbb3d38c755745b76c004da0f
Adam <adam.j.major@gmail.com>
2021-04-21 12:39:12 -0700

    start linked list implementation

--- LinkedList.js
+++ LinkedList.js
@@ -1,4 +1,22 @@
 "use strict";
 //My implementation of a LinkedList using TypeScript
+class LinkedListNode {
+    constructor(data) {
+        this.data = data;
+    }
+}
 class LinkedList {
+    // public tail: LinkedListNode<T>;
+    constructor(type) {
+        this.type = type;
+        this.head = LinkedListNode();
+    }
+    insert() {
+    }
+    insertAt() {
+    }
+    delete() {
+    }
+    deleteAt() {
+    }
 }
--- LinkedList.ts
+++ LinkedList.ts
@@ -1,5 +1,38 @@
 //My implementation of a LinkedList using TypeScript
+class LinkedListNode <T> {
+  public data: T;
+  public nextNode: any;
+  public previousNode: any;
 
-class LinkedList <T> {
-  
+  constructor(data: T) {
+    this.data = data;
+  }
 }
+class LinkedList <T> {
+  public type: InstanceTypes;
+  public head: LinkedListNode<T>;
+  // public tail: LinkedListNode<T>;
+
+  constructor(type: InstanceTypes) {
+    this.type = type;
+    this.head = LinkedListNode();
+  }
+
+  public insert(){
+
+  }
+
+  public insertAt() {
+
+  }
+
+  public delete() {
+
+  }
+
+  public deleteAt() {
+
+  }
+
+}
+
