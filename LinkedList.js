bfeef24e5825cd5ab8c82c98feda327b8d0fc00b
Adam <adam.j.major@gmail.com>
2021-04-21 18:05:30 -0700

    Add tail to LinkedList

--- LinkedList.js
+++ LinkedList.js
@@ -13,36 +13,78 @@ class LinkedList {
     constructor(type) {
         this._TYPE_ERROR = 'Incorect Type!';
         this._size = 0;
-        this.type = type;
-        this.head = new LinkedListNode(null);
+        this._type = type;
+        this._head = new LinkedListNode(null);
+        this._tail = new LinkedListNode(null);
     }
     // public tail: LinkedListNode<T>;
     get size() {
         return this._size;
     }
+    get type() {
+        return this._type;
+    }
+    get head() {
+        return this._head;
+    }
+    get tail() {
+        return this._tail;
+    }
     insert(data) {
         //Inserts at head O(1)
-        if (typeof data !== this.type)
+        if (typeof data !== this._type)
             return this._TYPE_ERROR;
-        if (this.head.data == null) {
-            return this.head.data = data;
+        if (this._head.data == null) {
+            this._size++;
+            this._tail.data = data;
+            return this._head.data = data;
         }
-        const previousHead = this.head;
+        //NOTE: THis code may be buggy
+        const previousHead = this._head;
         const newHead = new LinkedListNode(data);
         newHead.nextNode = previousHead;
         previousHead.previousNode = newHead;
-        this.head = newHead;
-        console.log('previousHead =', previousHead);
-        console.log('newHead =', newHead);
+        this._head = newHead;
+        if (this._size === 1) {
+            this._tail.previousNode = newHead;
+        }
         this._size++;
     }
-    insertAtIndex(data, index) {
-        if (typeof data !== this.type)
+    searchFromStart(data) {
+        if (typeof data !== this._type)
             return this._TYPE_ERROR;
-        if (this.head.data == null)
-            return this.head.data = data;
+        let traverser = this._head;
+        let count = 0;
+        while (traverser.data) {
+            if (data === traverser.data)
+                return count;
+            count++;
+            traverser = traverser.nextNode;
+        }
+        return -1;
+    }
+    searchFromEnd(data) {
+        if (typeof data !== this._type)
+            return this._TYPE_ERROR;
+        let traverser = this._tail;
+        let count = 0;
+        while (traverser.data) {
+            if (data === traverser.data)
+                return this._size - count;
+            count++;
+            traverser = traverser.previousNode;
+        }
+        return -1;
+    }
+    insertAtIndex(data, index) {
+        if (typeof data !== this._type)
+            return this._TYPE_ERROR;
+        if (this._head.data == null) {
+            this._size++;
+            return this._head.data = data;
+        }
         if (index > this._size - 1)
-            return this.tail;
+            return this._tail;
         this._size++;
     }
     delete() {
@@ -55,6 +97,8 @@ class LinkedList {
 const ll = new LinkedList(Stack_js_1.InstanceTypes.number);
 ll.insert(1);
 ll.insert(2);
-console.log('ll =', ll);
 ll.insert(3);
-ll.insert(4);
+console.log(ll);
+// console.log('ll.size =', ll.size);
+// ll.insert(3)
+// ll.insert(4)
--- LinkedList.ts
+++ LinkedList.ts
@@ -11,44 +11,91 @@ class LinkedListNode <T> {
 }
 class LinkedList <T> {
   private _TYPE_ERROR: string = 'Incorect Type!';
-  public type: InstanceTypes;
-  public head: LinkedListNode <T>;
+  private _type: InstanceTypes;
+  private _head: LinkedListNode <T>;
+  private _tail: LinkedListNode <T>;
   private _size: number = 0;
 
   // public tail: LinkedListNode<T>;
-
+  
   get size() {
     return this._size;
   }
+  get type() {
+    return this._type;
+  }
+  get head() {
+    return this._head;
+  }
+  get tail() {
+    return this._tail;
+  }
 
   constructor(type: InstanceTypes) {
-    this.type = type;
-    this.head = new LinkedListNode(null);
+    this._type = type;
+    this._head = new LinkedListNode(null) as any;
+    this._tail = new LinkedListNode(null) as any;
   }
 
   public insert(data: T){
     //Inserts at head O(1)
-    if (typeof data !== this.type) return this._TYPE_ERROR;
-    if (this.head.data == null) {
-      return this.head.data = data;
+    if (typeof data !== this._type) return this._TYPE_ERROR;
+    if (this._head.data == null) {
+      this._size++;
+      this._tail.data = data;
+      return this._head.data = data;
     }
-
-    const previousHead = this.head;
+    
+    //NOTE: THis code may be buggy
+    const previousHead = this._head;
     const newHead = new LinkedListNode(data);
     newHead.nextNode = previousHead;
     previousHead.previousNode = newHead;
-    this.head = newHead;
-    console.log('previousHead =', previousHead);
-    console.log('newHead =', newHead);
+    this._head = newHead;
+
+    if (this._size === 1) {
+      this._tail.previousNode = newHead;
+    }
+
     this._size++;
   }
 
+  public searchFromStart(data: T) {
+    if (typeof data !== this._type) return this._TYPE_ERROR;
+    
+    let traverser = this._head;
+    let count = 0
+    while (traverser.data) {
+      if (data === traverser.data) return count;
+      count++;
+      traverser = traverser.nextNode;
+    }
+    return -1;
+  }
+
+  public searchFromEnd(data: T) {
+    if (typeof data !== this._type) return this._TYPE_ERROR;
+    
+    let traverser = this._tail;
+    let count = 0
+    while (traverser.data) {
+      if (data === traverser.data) return this._size - count;
+      count++;
+      traverser = traverser.previousNode;
+    }
+    return -1;
+  }
+
   public insertAtIndex(data: T, index: number) {
-    if (typeof data !== this.type) return this._TYPE_ERROR;
-    if (this.head.data == null) return this.head.data = data;
-    if (index > this._size - 1) return this.tail
+    if (typeof data !== this._type) return this._TYPE_ERROR;
+    if (this._head.data == null) {
+      this._size++; 
+      return this._head.data = data;
+    }
+    if (index > this._size - 1) return this._tail
 
     this._size++;
+
   }
 
   public delete() {
@@ -64,6 +111,10 @@ class LinkedList <T> {
 const ll = new LinkedList(InstanceTypes.number);
 ll.insert(1)
 ll.insert(2)
-console.log('ll =', ll);
 ll.insert(3)
-ll.insert(4)
+console.log(ll)
+
+// console.log('ll.size =', ll.size);
+
+// ll.insert(3)
+// ll.insert(4)
